a912eebb4cc206259dc1b7c2abd5c99965f83288
Lev McKinney <levmckinney@gmail.com>
2025-12-04 09:38:47 +0000

    more data

--- examples/qwen_scaling/fit_factors_batch_test.sh
+++ examples/qwen_scaling/fit_factors_batch_test.sh
@@ -5,7 +5,7 @@
 # Uses accelerate launch with FSDP enabled
 
 # Configuration
-MODEL_SIZES=("1.5B 0.5B")
+MODEL_SIZES=("1.5B" "0.5B")
 NUM_TRAIN_SAMPLES=1000
 
 # Number of GPUs to use (adjust as needed)
